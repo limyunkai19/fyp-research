6062d5832536605d3b2f260095f768ab701b5fe9
limyunkai19 <yunkai96@hotmail.com>
2018-04-13 11:51:57 +0800

    Fix several bugs in script

--- scripts/dataset_similarity.sh
+++ scripts/dataset_similarity.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-# dataset=stl10 cifar10 emnist
+dataset=(stl10 cifar10 emnist)
 datasetSize=(80 80 17)
 model=resnet18
 
-for dataset in stl10 cifar10 emnist; do
+for d in 0 1 2; do
     for i in 0 2 4
     do
-        python -u experiment.py $model $i $dataset dataset_similarity_${dataset}_${model}_${i} --sample-per-class ${datasetSize[$i]} --batch-size 64 --epochs 40 --data-augmentation &> dataset_similarity_${dataset}_${model}_${i}
-        mv dataset_similarity_${dataset}_${model}_${i} results/dataset_similarity_${dataset}_${model}_${i}/output.txt
+        python -u experiment.py $model $i ${dataset[$d]} dataset_similarity_${dataset[$d]}_${model}_${i} --sample-per-class ${datasetSize[$d]} --batch-size 64 --epochs 40 --data-augmentation &> dataset_similarity_${dataset[$d]}_${model}_${i}
+        mv dataset_similarity_${dataset[$d]}_${model}_${i} results/dataset_similarity_${dataset[$d]}_${model}_${i}/output.txt
     done;
-    exp=dataset_similarity_${dataset}_${model}_-1,dataset_similarity_${dataset}_${model}_1,dataset_similarity_${dataset}_${model}_2,dataset_similarity_${dataset}_${model}_3,dataset_similarity_${dataset}_${model}_4
-    python plot_graph.py $exp "scratch,pretrained: 0,pretrained: 1,pretrained: 2,pretrained: 3,pretrained: 4" "$model on $dataset dataset with data augmentation"
-    mv ${model}_on_${dataset}_dataset_with_data_augmentation.png results/graphs/dataset_similarity_${dataset}_${model}.png
+    exp=dataset_similarity_${dataset[$d]}_${model}_0,dataset_similarity_${dataset[$d]}_${model}_2,dataset_similarity_${dataset[$d]}_${model}_4
+    python plot_graph.py $exp "pretrained: 0,pretrained: 2,pretrained: 4" "$model on ${dataset[$d]} dataset with data augmentation"
+    mv ${model}_on_${dataset[$d]}_dataset_with_data_augmentation.png results/graphs/dataset_similarity_${dataset[$d]}_${model}.png
 done;
 
--- scripts/layer_transferred.sh
+++ scripts/layer_transferred.sh
@@ -9,7 +9,7 @@ for model in alexnet vgg16_bn densenet121 resnet50; do
         python -u experiment.py $model $i $dataset layer_transferred_${dataset}_${model}_${i} --batch-size 32 --epochs 20 --data-augmentation &> layer_transferred_${dataset}_${model}_${i}
         mv layer_transferred_${dataset}_${model}_${i} results/layer_transferred_${dataset}_${model}_${i}/output.txt
     done;
-    exp=layer_transferred_${dataset}_${model}_-1,layer_transferred_${dataset}_${model}_1,layer_transferred_${dataset}_${model}_2,layer_transferred_${dataset}_${model}_3,layer_transferred_${dataset}_${model}_4
+    exp=layer_transferred_${dataset}_${model}_-1,layer_transferred_${dataset}_${model}_0,layer_transferred_${dataset}_${model}_1,layer_transferred_${dataset}_${model}_2,layer_transferred_${dataset}_${model}_3,layer_transferred_${dataset}_${model}_4
     python plot_graph.py $exp "scratch,pretrained: 0,pretrained: 1,pretrained: 2,pretrained: 3,pretrained: 4" "$model on $dataset dataset with data augmentation"
     mv ${model}_on_${dataset}_dataset_with_data_augmentation.png results/graphs/layer_transferred_${dataset}_${model}.png
 done;
