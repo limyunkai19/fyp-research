4103fc14f5c0b6210da4ccaebc24c220f2ac07fb
limyunkai19 <yunkai96@hotmail.com>
2018-04-13 11:38:05 +0800

    Fix typo

--- experiment.py
+++ experiment.py
@@ -14,7 +14,7 @@ parser.add_argument('--batch-size', type=int, default=64, metavar='N',
 parser.add_argument('--epochs', type=int, default=10, metavar='N',
                     help='number of epochs to train (default: 10)')
 parser.add_argument('--lr', type=float, default=1e-4, metavar='LR',
-                    help='learning rate (default: 0.001)')
+                    help='learning rate (default: 0.0001)')
 parser.add_argument('model', help='name of model used for training')
 parser.add_argument('pretrained', type=int, help='level knowledge of transferred from pretrained')
 parser.add_argument('dataset', help='dataset for experiemnt')
